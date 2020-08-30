7a15d1534d94b1a990ca9918e8134408996c281d
manojmadhav2000 <mini.project20001@gmail.com>
2020-08-30 17:24:22 +0530

    text input to read file Cleaning Text using String Punctuations

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import string
+
 text_file = open("read.txt", "wt")
-text_input = input()
+print("Enter string to write to text file.")
+text_input = input().lower()
 n = text_file.write(text_input)
 text_file.close()
+
+# Cleaning Texts
+
+text = open('read.txt', encoding='utf-8', ).read()
+cleaned_text = text.translate(str.maketrans('', '', string.punctuation))
+print(cleaned_text)
