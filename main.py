237d98a794c6a7a9cf42f482b8fc9f45b8726448
Madhav246 <madhav.agrawal_cs18@gla.ac.in>
2020-10-19 22:33:05 +0530

    Importing NLTK

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import string
 from collections import Counter
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 import matplotlib.pyplot as plt
 
 text_file = open("read.txt", "wt")
@@ -13,28 +15,16 @@ text_file.close()
 text = open('read.txt', encoding='utf-8', ).read()
 cleaned_text = text.translate(str.maketrans('', '', string.punctuation))
 
-# Tokenization
+# Tokenization using NLTK
 
-tokenized_words = cleaned_text.split()
-print(tokenized_words)
-
-stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
-              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
-              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
-              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
-              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
-              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
-              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
-              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
-              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
-              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+tokenized_words = word_tokenize(cleaned_text, "english")
 
 final_words = []
 for word in tokenized_words:
-    if word not in stop_words:
+    if word not in stopwords.words('english'):
         final_words.append(word)
 
-# counting emotions
+# Counting emotions
 
 emotion_list = []
 with open('emotions.txt', 'r') as file:
