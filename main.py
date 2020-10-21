0af4fee3f850c8d70c82dc55489c2d416fcb30ae
Madhav246 <madhav.agrawal_cs18@gla.ac.in>
2020-10-21 12:45:16 +0530

    Sentiment analysis of a given text Completed!

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from collections import Counter
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import matplotlib.pyplot as plt
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 text_file = open("read.txt", "wt")
 print("Enter string to write to text file.")
@@ -12,7 +13,7 @@ text_file.close()
 
 # Cleaning Texts
 
-text = open('read.txt', encoding='utf-8', ).read()
+text = open('read.txt', encoding='utf-8').read()
 cleaned_text = text.translate(str.maketrans('', '', string.punctuation))
 
 # Tokenization using NLTK
@@ -38,6 +39,22 @@ with open('emotions.txt', 'r') as file:
 print(emotion_list)
 w = Counter(emotion_list)
 print(w)
+
+
+# Getting overall Sentiment
+def sentiment_analyse(sentiment_text):
+    score = SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
+    neg = score['neg']
+    pos = score['pos']
+    if neg > pos:
+        print("Negative Sentiment")
+    elif pos > neg:
+        print("Positive Sentiment")
+    else:
+        print("Neutral")
+
+
+sentiment_analyse(cleaned_text)
 
 # Graphical Representation of data
 
